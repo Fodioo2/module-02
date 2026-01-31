041b623c18b33f709293e7a4b14b9ddf1cb50448
Fodi_002 <eldosdid24@gmail.com>
2026-01-31 17:52:13 +0500

    Added module 02 tasks

---
+++ HomeWork/DRY/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ln1
+{
+    class program
+    {
+        public void Method(string type, string message)
+        {
+            Console.WriteLine(type + ": " + message);
+        }
+        static void Main()
+        {
+            program p = new program();
+            p.Method("Info", "This is a sample message.");
+            p.Method("Warning", "This is a warning message.");
+            p.Method("Error", "This is an error message.");
+
+        }
+    }
+}
